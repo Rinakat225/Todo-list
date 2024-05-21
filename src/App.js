0762f3f8a738ac46e95d4e0e1cf0792dea8a17a1
Rinakat225 <katarinacala@gmail.com>
2024-05-21 10:25:41 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [userInput, setUserInput] = useState([]);
+
+  function handleAddNewTask(task) {
+    setUserInput((previousTasks) => [...previousTasks, task]);
+  }
+
+  function handleClearAll() {
+    setUserInput([]);
+  }
+
+  function handleRemoveTask(id) {
+    setUserInput((newTasks) => newTasks.filter((newTask) => newTask.id !== id));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div>
+        <NewTasks onAddNewTask={handleAddNewTask} onClearAll={handleClearAll} />
+      </div>
+      <div>
+        {userInput.map((input) => (
+          <TaskList
+            userInput={input.newTask}
+            id={input.id}
+            key={input.id}
+            onRemoveTask={handleRemoveTask}
+          />
+        ))}
+      </div>
     </div>
   );
 }
 
-export default App;
+function NewTasks({ onAddNewTask, onClearAll }) {
+  const [newTask, setNewTask] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!newTask) return;
+
+    const task = { newTask, id: Date.now() };
+
+    onAddNewTask(task);
+
+    setNewTask("");
+  }
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Add your task"
+          value={newTask}
+          onChange={(event) => setNewTask(event.target.value)}
+        />
+        <button className="submit--btn">Submit</button>
+        <button onClick={() => onClearAll()}>Clear all</button>
+      </form>
+    </div>
+  );
+}
+
+function TaskList({ userInput, id, onRemoveTask }) {
+  return (
+    <div className="tasklist--container">
+      <ul>
+        <li>{userInput}</li>
+      </ul>
+      <button onClick={() => onRemoveTask(id)}>Delete</button>
+    </div>
+  );
+}
