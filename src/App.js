ed01f31e3d3b6230d45b4894479626b9bde161db
Rinakat225 <katarinacala@gmail.com>
2024-05-24 10:12:21 +0200

    Configured ESLint and Prettier

--- src/App.js
+++ src/App.js
@@ -1,78 +1,40 @@
-import { useState } from "react";
+import {useState} from 'react';
+import NewTasks from './components/NewTasks';
+import TaskList from './components/TaskList';
 
 export default function App() {
-  const [userInput, setUserInput] = useState([]);
+    const [userInput, setUserInput] = useState([]);
 
-  function handleAddNewTask(task) {
-    setUserInput((previousTasks) => [...previousTasks, task]);
-  }
+    function handleAddNewTask(task) {
+        setUserInput((previousTasks) => [...previousTasks, task]);
+    }
 
-  function handleClearAll() {
-    setUserInput([]);
-  }
+    function handleClearAll() {
+        setUserInput([]);
+    }
 
-  function handleRemoveTask(id) {
-    setUserInput((newTasks) => newTasks.filter((newTask) => newTask.id !== id));
-  }
+    function handleRemoveTask(id) {
+        setUserInput((tasks) => tasks.filter((task) => task.id !== id));
+    }
 
-  return (
-    <div className="container">
-      <div>
-        <NewTasks onAddNewTask={handleAddNewTask} onClearAll={handleClearAll} />
-      </div>
-      <div>
-        {userInput.map((input) => (
-          <TaskList
-            userInput={input.newTask}
-            id={input.id}
-            key={input.id}
-            onRemoveTask={handleRemoveTask}
-          />
-        ))}
-      </div>
-    </div>
-  );
-}
-
-function NewTasks({ onAddNewTask, onClearAll }) {
-  const [newTask, setNewTask] = useState("");
-
-  function handleSubmit(e) {
-    e.preventDefault();
-
-    if (!newTask) return;
-
-    const task = { newTask, id: Date.now() };
-
-    onAddNewTask(task);
-
-    setNewTask("");
-  }
-
-  return (
-    <div>
-      <h1>Todo List</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Add your task"
-          value={newTask}
-          onChange={(event) => setNewTask(event.target.value)}
-        />
-        <button className="submit--btn">Submit</button>
-        <button onClick={() => onClearAll()}>Clear all</button>
-      </form>
-    </div>
-  );
-}
-
-function TaskList({ userInput, id, onRemoveTask }) {
-  return (
-    <div className="tasklist--container">
-      <ul>
-        <li>{userInput}</li>
-      </ul>
-      <button onClick={() => onRemoveTask(id)}>Delete</button>
-    </div>
-  );
+    return (
+        <div className="bg-blue-500 text-black p-4">
+            <div>
+                <NewTasks
+                    onAddNewTask={handleAddNewTask}
+                    onClearAll={handleClearAll}
+                />
+            </div>
+            <div>
+                {userInput.map((input) => (
+                    <TaskList
+                        userInput={input.newTask}
+                        id={input.id}
+                        key={input.id}
+                        onRemoveTask={handleRemoveTask}
+                    />
+                ))}
+            </div>
+        </div>
+    );
 }
